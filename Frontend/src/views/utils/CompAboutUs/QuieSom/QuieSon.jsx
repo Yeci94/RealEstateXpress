20d0cf8ca11b98684b54d7b863104c1cb590d6ad
diana <diana7170368@gmail.com>
2024-09-17 21:38:55 -0500

    "integrando"

--- Frontend/src/views/utils/CompAboutUs/QuieSom/QuieSon.jsx
+++ Frontend/src/views/utils/CompAboutUs/QuieSom/QuieSon.jsx
@@ -1,48 +1,87 @@
-import './QuieSon.css';
+import "./QuieSon.css";
 
 function QuieSon() {
-    return (
-        <section className="about-us py-5 bg-light">
-            <div className="container">
-                <h2 className="text-center mb-4">Acerca de Nosotros</h2>
-                <div className="row align-items-center">
-                    <div className="col-md-6">
-                        <h4>¿Quiénes Somos?</h4>
-                        <p>
-                            Somos un equipo dedicado a trasformar la manera en que se adquieren terrenos en florida, creemos que invertir en bienes raices debe ser un proceso sencillo, seguro y accesible. Por eso desarrolamos una plataforma intuitiva y novedosa donde nuestros clientes compren 100% oline. faciltando todo el proceso de desde la seleccion del terreno hasta la recepcion de la propiedad.
-                        </p>
-
-                    </div>
-                    <div className="col-md-6">
-                        <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <img src="Img/1.jpg" className="d-block w-100" alt="Propiedad 1" />
-                                </div>
-                                <div className="carousel-item">
-                                    <img src="Img/5.jpg" className="d-block w-100" alt="Propiedad 2" />
-                                </div>
-                                <div className="carousel-item">
-                                    <img src="Img/3.jpg" className="d-block w-100" alt="Propiedad 3" />
-                                </div>
-                                <div className="carousel-item">
-                                    <img src="Img/4.jpg" className="d-block w-100" alt="Propiedad 3" />
-                                </div>
-                            </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Previous</span>
-                            </button>
-                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                    </div>
+  return (
+    <section className="about-us py-5 bg-light">
+      <div className="container">
+        <h2 className="text-center mb-4">Acerca de Nosotros</h2>
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <h4>¿Quiénes Somos?</h4>
+            <p>
+              Somos un equipo dedicado a transformar la manera en que se
+              adquieren terrenos en florida, creemos que invertir en bienes
+              raíces debe ser un proceso sencillo, seguro y accesible. Por eso,
+              desarrollamos una plataforma intuitiva y novedosa donde nuestros
+              clientes compren 100% oline. Facilitando todo el proceso desde
+              la selección del terreno hasta la recepción de la propiedad.
+            </p>
+          </div>
+          <div className="col-md-6">
+            <div
+              id="carouselExample"
+              className="carousel slide"
+              data-bs-ride="carousel"
+            >
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img
+                    src="Img/1.jpg"
+                    className="d-block w-100"
+                    alt="Propiedad 1"
+                  />
                 </div>
+                <div className="carousel-item">
+                  <img
+                    src="Img/5.jpg"
+                    className="d-block w-100"
+                    alt="Propiedad 2"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="Img/3.jpg"
+                    className="d-block w-100"
+                    alt="Propiedad 3"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="Img/4.jpg"
+                    className="d-block w-100"
+                    alt="Propiedad 3"
+                  />
+                </div>
+              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExample"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExample"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
             </div>
-        </section>
-
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default QuieSon;
