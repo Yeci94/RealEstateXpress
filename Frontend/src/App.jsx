07d5e3f66c34fa98ea52653def81e8652e923f72
diana <diana7170368@gmail.com>
2024-09-22 12:10:08 -0500

    ordenando App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,12 @@
-import './App.css'
-import QuieSon from './views/utils/CompAboutUs/QuieSom/QuieSon';
-import Footer from './views/utils/Footer/Footer'
-import Nav from "./views/utils/Nav/Nav"
-import MisVis from "./views/utils/CompAboutUs/MisVis/MisVis"
-import Valores from "./views/utils/CompAboutUs/Valores/Valores"
-import Integrantes from './views/utils/CompAboutUs/Integrantes/Integrantes';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./App.css";
+import AboutUs from "./views/AboutUs/AboutUs";
 
 function App() {
-
   return (
     <>
-      <Nav/>
-      <div class="contenedor-app">
-      <QuieSon/>
-      <MisVis/>
-      <Valores/>
-      <br></br>
-      <br></br>
-      <Integrantes/>
-      <Footer/>
-      </div>
+      <AboutUs />
     </>
-  )
+  );
 }
 
 export default App;
